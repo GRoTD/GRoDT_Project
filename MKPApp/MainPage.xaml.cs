a1cc579af883aa711f6a2c03b85d316f6fce9103
Johan Fahlgren <johan.fahlgren@utb.ecutbildning.se>
2022-04-04 20:44:37 +0200

    Add project files.

---
+++ MKPApp/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MKPApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MKPApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MKPApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
