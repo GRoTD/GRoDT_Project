14e0d51cbd312c7c31c132a0f09cde064254d5c0
Kim Björnsen Åklint <89244587+Bjornsen016@users.noreply.github.com>
2022-05-03 17:57:22 +0200

    Fixed so that we can start with App and Web programming.

--- MKPApp/MauiProgram.cs
+++ MKPApp/MauiProgram.cs
@@ -5,20 +5,16 @@ namespace MKPApp;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.RegisterBlazorMauiWebView()
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts => { fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"); });
 
-		builder.Services.AddBlazorWebView();
-		builder.Services.AddSingleton<WeatherForecastService>();
+        builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddSingleton<WeatherForecastService>();
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
