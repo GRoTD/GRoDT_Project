78a2c03bb1331327be805c85adf6abf025c45b44
Kim Björnsen Åklint <89244587+Bjornsen016@users.noreply.github.com>
2022-05-03 17:57:57 +0200

    Added some controller shells to start of with

---
+++ SlippAPI/Controllers/ClubController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SlippAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClubController : ControllerBase
+    {
+        #region Auctions
+
+        [HttpGet]
+        [Route("auctions")]
+        public async Task<ActionResult> GetAuctions()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("auctions/{id}")]
+        public async Task<ActionResult> GetAuction(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Employees
+
+        [HttpGet]
+        [Route("employees/{id}")]
+        public async Task<ActionResult> GetEmployee(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+
+        #region Tickets
+
+        #endregion
+    }
+}
