25f9d81e7569e115051f4dcdf59095735347dd87
Pia Hagman <pia.hagman@utb.ecutbildning.se>
2022-06-23 23:49:51 +0200

    Starts create ClubsOverview. Addded DTO ClubOutput as well as clubs.json to make it work.

---
+++ Slipp.Services/DTO/ClubOutput.cs
@@ -0,0 +1,37 @@
+﻿using Slipp.Services.Models;
+using System.Text.Json.Serialization;
+
+namespace Slipp.Services.DTO;
+
+public class ClubOutput
+{
+    [JsonPropertyName("id")] public Guid Id { get; set; }
+    [JsonPropertyName("name")] public string Name { get; set; }
+    [JsonPropertyName("address")] public string Address { get; set; }
+    [JsonPropertyName("website")] public string Website { get; set; }
+    [JsonPropertyName("longitude")] public double Longitude { get; set; }
+    [JsonPropertyName("latitude")] public double Latitude { get; set; }
+    [JsonPropertyName("description")] public string Description { get; set; }
+    [JsonPropertyName("company")] public Company Company { get; set; }
+    [JsonPropertyName("images")] public List<Image> Images { get; set; }
+
+
+    [JsonPropertyName("tickets")] public List<Ticket>? Tickets { get; set; }
+    [JsonPropertyName("auctions")] public List<Auction>? Auctions { get; set; }
+
+    //public static ClubOutput Create(Club club)
+    //{
+    //    var outputClub = new ClubOutput
+    //    {
+    //        Name = club.Name,
+    //        Address = club.Adress,
+    //        Website = club.Website,
+    //        Company = club.Company,
+    //        Longitude = club.Longitude,
+    //        Latitude = club.Latitude,
+    //        Description = club.Description,
+
+    //    };
+    //    return outputClub;
+    //}
+}
