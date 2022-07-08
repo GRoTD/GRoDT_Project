aac2567cb5608e028e4a57ab9bcbc215864538e5
Kim Björnsen Åklint <89244587+Bjornsen016@users.noreply.github.com>
2022-07-08 21:53:03 +0200

    commit trying to fix API publish issues

---
+++ SlippAPI/Controllers/DatabaseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SlippAPI.Controllers;
+
+[Route("api/database")]
+[ApiController]
+public class DatabaseController : ControllerBase
+{
+    private readonly Database _db;
+
+    public DatabaseController(Database db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    [Authorize]
+    [Route("resetDb")]
+    public async Task<ActionResult> ResetDb()
+    {
+        await _db.RecreateAndSeed();
+        return Ok();
+    }
+}
