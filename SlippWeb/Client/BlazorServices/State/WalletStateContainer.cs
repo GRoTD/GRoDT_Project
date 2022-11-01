1c50f1bad18b3f9b919b3fdf3937213550833eda
Kim Björnsen Åklint <89244587+Bjornsen016@users.noreply.github.com>
2022-11-01 21:57:47 +0100

    Restructuring and start of repairing old functionality. This version is not fully working. See Payment.razor

---
+++ SlippWeb/Client/BlazorServices/State/WalletStateContainer.cs
@@ -0,0 +1,23 @@
+﻿using Slipp.Services.DTO;
+
+namespace SlippWeb.Client.BlazorServices.State;
+
+public class ShoppingCartStateContainer
+{
+    private List<TicketOutput> _currentTickets;
+
+    public List<TicketOutput> CurrentTickets
+    {
+        get => _currentTickets;
+        set
+        {
+            _currentTickets = value;
+            NotifyStateChanged();
+        }
+    }
+
+
+    public event Action? OnChange;
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
